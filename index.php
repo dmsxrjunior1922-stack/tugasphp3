daa2cd81316896acf96eebeb485d264c037dedd2
AHMAD ADNAN NUR HIKMAH_1922 <dmsxrjunior1922@gmail.com>
2025-11-21 16:26:20 +0700

    Refactor index.php for number checks and display
    Updated PHP script to check if a number is odd or even and display numbers from 100 to 0.

--- index.php
+++ index.php
@@ -1,17 +1,24 @@
 <?php
-$angka = 15;
+// --------------------------------------
+// 1. Mengecek satu angka (ganjil/genap)
+// --------------------------------------
+$angka = 15; // contoh angka
+
+echo "<h3>Pengecekan Satu Angka</h3>";
+
 if ($angka % 2 == 0) {
-    echo "$angka adalah bilangan genap";
+    echo "$angka adalah bilangan Genap<br><br>";
 } else {
-    echo "$angka adalah bilangan ganjil";
+    echo "$angka adalah bilangan Ganjil<br><br>";
 }
 
-?>
 
+// --------------------------------------
+// 2. Menampilkan 100 sampai 0 (ganjil/genap)
+// --------------------------------------
+echo "<h3>Daftar 100 sampai 0</h3>";
 
-
-<?php
-for ($i = 1; $i <= 100; $i++) {
+for ($i = 100; $i >= 0; $i--) {
     if ($i % 2 == 0) {
         echo "$i adalah Genap<br>";
     } else {
